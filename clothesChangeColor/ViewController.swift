30628beb7baa129396ba2090b887ed183cf79e83
Vic <ji3g4vic03@gmail.com>
2023-02-05 15:53:03 +0800

    random color

--- clothesChangeColor/ViewController.swift
+++ clothesChangeColor/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+@IBOutlet weak var redSilder: UISlider!
+@IBOutlet weak var greenSilder: UISlider!
+@IBOutlet weak var alphaSilder: UISlider!
+@IBOutlet weak var blueSilder: UISlider!
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        backgroundImageView.backgroundColor = UIColor.systemMint
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeColor(_ sender: Any) {
+        backgroundImageView.backgroundColor = UIColor(red: CGFloat(redSilder.value), green: CGFloat(greenSilder.value), blue: CGFloat(blueSilder.value), alpha: CGFloat(alphaSilder.value))
+    }
+    
+    @IBAction func random(_ sender: Any) {
+        redSilder.setValue(Float.random(in: 0...1), animated: true)
+        greenSilder.setValue(Float.random(in: 0...1), animated: true)
+        blueSilder.setValue(Float.random(in: 0...1), animated: true)
+        alphaSilder.setValue(Float.random(in: 0...1), animated: true)
+        backgroundImageView.backgroundColor = UIColor(red: CGFloat(redSilder.value), green: CGFloat(greenSilder.value), blue: CGFloat(blueSilder.value), alpha: CGFloat(alphaSilder.value))
 
+    }
 }
 
